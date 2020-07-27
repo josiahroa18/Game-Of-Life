2a5d7250103a98e9fe908a769467753d84ac2481
Josiah Roa <josiahroa18@gmail.com>
2020-07-27 13:07:11 -0600

    Updated style for grid

--- client/src/components/styles.js
+++ client/src/components/styles.js
@@ -159,7 +159,7 @@ export const Highlight = styled.span`
     color: ${theme.colors.lightGreen};
 `;
 
-// =========== Grid Styles ===========
+// =========== Stats Styles ===========
 export const StatsWrapper = styled.div`
     width: 100%;
     display: flex;
@@ -193,7 +193,10 @@ export const Grid = styled.div`
 export const Cell = styled.div`
     width: 20px;
     height: 20px;
-    background-color: ${props => props.live ? theme.colors.black : undefined};
-    border: 1px solid ${theme.colors.black};
+    background-color: ${props => props.live ? undefined : theme.colors.black};
+    // border: 1px solid ${theme.colors.black};
     cursor: pointer;
+    &:hover{
+        background-color: ${theme.colors.white};
+    }
 `;
